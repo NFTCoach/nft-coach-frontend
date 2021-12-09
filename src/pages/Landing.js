c101334b7894ae6264d7a6b3ae8be431fb4b421f
asgarovf <farhad@getwhelp.com>
2021-12-09 23:37:34 +0300

    init: landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,9 +3,17 @@ import styles from "./Landing.module.scss";
 
 const Landing = () => {
   return (
-    <div className={styles.hello}>
-      <Typography variant="body2" weight="medium">
-        asfafs
+    <div className={styles.hero}>
+      <Typography className={styles.header} variant="title1" weight="semibold">
+        NFT COACH
+      </Typography>
+      <Typography
+        className={styles.subheader}
+        variant="title5"
+        weight="regular"
+      >
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form, by injected humour,
       </Typography>
     </div>
   );
