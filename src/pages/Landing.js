0ca44f781dbb3144c2d753d7ae358cdfcda3b343
asgarovf <farhad@getwhelp.com>
2021-12-09 21:28:58 +0300

    init: landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,14 @@
+import { Typography } from "components/Typography";
 import styles from "./Landing.module.scss";
 
 const Landing = () => {
-  return <div className={styles.hello}>asfasf</div>;
+  return (
+    <div className={styles.hello}>
+      <Typography variant="body2" weight="medium">
+        asfafs
+      </Typography>
+    </div>
+  );
 };
 
 export { Landing };
